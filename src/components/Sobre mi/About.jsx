2b0185f1577ff603934d6f22625f6fcf62cc6727
Walther Matute <magawaja5454@gmail.com>
2025-03-05 18:59:31 -0500

    Actualizacion

--- src/components/Sobre mi/About.jsx
+++ src/components/Sobre mi/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
                     <h1>Sobre <span>mi</span></h1>
                     <div className="about-p-image">
                         <img src={Perfil} alt="" />
-                        <p>¡Hola! Soy Walther Jair Matute Garcia, tengo 19 años y me apasiona la tecnología, especialmente la programación en entornos front-end y backend. También me interesa la administración de sistemas operativos. Mi proceso de aprendizaje comenzó desde la secundaria, y actualmente estoy estudiando <span>Ingeniería en Tecnologías de la Información.</span> Me encanta seguir aprendiendo y aplicando mis conocimientos en proyectos que me permitan mejorar mis habilidades. Actualmente, me desempeño como <span>programador junior,</span> siempre buscando nuevos retos y oportunidades para crecer profesionalmente.</p>
+                        <p>¡Hola! Soy Walther Jair Matute Garcia, tengo 20 años y me apasiona la tecnología, especialmente la programación en entornos front-end y backend. También me interesa la administración de sistemas operativos. Mi proceso de aprendizaje comenzó desde la secundaria, y actualmente estoy estudiando <span>Ingeniería en Tecnologías de la Información.</span> Me encanta seguir aprendiendo y aplicando mis conocimientos en proyectos que me permitan mejorar mis habilidades. Actualmente, me desempeño como <span>programador junior,</span> siempre buscando nuevos retos y oportunidades para crecer profesionalmente.</p>
                     </div>
                 </div>
             </section>
